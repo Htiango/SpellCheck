99e573877dafb6838159d3fa0444b34b406d5008
Htiango <hongty106@gmail.com>
2016-09-24 21:59:41 +0800

    3rd commit. Add beam search to the function

--- SpellCheck/levenDis.cpp
+++ SpellCheck/levenDis.cpp
@@ -39,7 +39,6 @@ unsigned int levenshteinDis(const string str_input, const string str_tem){
             if (temp_down > len_tem) {
                 temp_down = len_tem;
             }
-//            col[temp_down] = min({prevCol[temp_down] + 1, prevCol[temp_down - 1] + (str_input[i - 1] == str_tem[temp_down - 1] ? 0 : 1)});
         }
             
         temp_up += 1;
@@ -63,16 +62,68 @@ unsigned int levenshteinDis(const string str_input, const string str_tem){
                 }
             }
         }
-        
         col.swap(prevCol);
-        
     }
+    return prevCol[len_tem];
+}
+
+
+// get the levenshteinDis (use absolute edit distance pruning)
+unsigned int beamLevenshtein(const string str_input, const string str_tem){
+    
+    const size_t len_input = str_input.size(), len_tem = str_tem.size();
+    vector<unsigned int > col(len_tem + 1), prevCol(len_tem + 1), colMax(len_tem + 1);
+    
+    unsigned int temp_up = len_tem - 1;
+    
+    unsigned int bestCost;
+    
+    // initialize the max column
+    for (unsigned int i = 0; i < len_tem + 1; i++) {
+        colMax[i] = UINT_MAX / 2;
+    }
+
+    prevCol = colMax;    // initialize the first column
+    
+    // re-initialize the first column
+    for(unsigned int i = 0; i < len_tem + 1; i++)
+    {
+        if (i > BEAM){
+            temp_up = i;
+            break;
+        }
+        prevCol[i] = i;
+    }
+    
+    for(unsigned int i = 0; i < len_input ; i++){
+        col = colMax;
+        
+        col[0] = prevCol[0] + 1;
+        bestCost = col[0];
+        for (unsigned int j = 0; j < len_tem; j ++) {
+            if (j < temp_up + 1){
+                col[j + 1] = min({ prevCol[1 + j] + 1, col[j] + 1, prevCol[j] + (str_input[i] == str_tem[j] ? 0 : 1)});
+            }
+            else{
+                col[j + 1] = col[j] + 1;
+            }
+            if (col[j + 1] < bestCost) {
+                bestCost = col[j + 1];
+            }
+            else if (col[j + 1] - bestCost > BEAM){
+                temp_up = j + 1;
+                break;
+            }
+        }
+        col.swap(prevCol);
+    }
+    
     
     return prevCol[len_tem];
 }
 
 // brute force
-unsigned int levenshtein_distance(const std::string& s1, const std::string& s2)
+unsigned int resursiveLevenshtein(const std::string& s1, const std::string& s2)
 {
     const std::size_t len1 = s1.size(), len2 = s2.size();
     std::vector<unsigned int> col(len2+1), prevCol(len2+1);
--- SpellCheck/levenDis.h
+++ SpellCheck/levenDis.h
@@ -11,8 +11,9 @@ using namespace std;
 #define EDIT_DIS        (3)
 #define BEAM            (3)
 
-unsigned int levenshteinDis(const string str_input, const string str_tem);
-unsigned int levenshtein_distance(const std::string& s1, const std::string& s2);
+unsigned int levenshteinDis(const string str_input, const string str_tem);   // pruning use the edit distance
+unsigned int beamLevenshtein(const string str_input, const string str_tem);   // pruning use the beam search
+unsigned int resursiveLevenshtein(const std::string& s1, const std::string& s2);   // brute force
 
 #endif
 
